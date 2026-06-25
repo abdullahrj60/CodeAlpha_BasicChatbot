a2e94e2478fbfeaaf9344acd21caf5b009c830b8
Muhmmad Abdullah <abdullah.imtiaz60@gmail.com>
2026-06-26 01:51:18 +0500

    Create basic chatbot loop
    Added a basic chatbot implementation with a start function.

--- chatbot.py
+++ chatbot.py
@@ -1,1 +1,15 @@
-print("Hello, I am a chatbot!")
+# basic chatbot 
+
+def start_chatbot():
+    print("Welcome to PyBot")
+    print("Type 'bye' to end the chat.")
+
+    while True:
+        message = input("You: ")
+
+        if message.lower() == "bye":
+            print("PyBot: Goodbye!")
+            break
+
+
+start_chatbot()
